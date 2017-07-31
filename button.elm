2ff0d91f665b8897de37fd3d415969a50941ca27
ash <ashishg@qburst.com>
2017-07-31 11:39:50 +0530

    Format button.elm

--- button.elm
+++ button.elm
@@ -1,27 +1,46 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main = Html.beginnerProgram { model = model, view = view, update = update }
 
-type alias Model = Int
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+type alias Model =
+    Int
+
 
 model : Model
-model = 0
+model =
+    0
 
-type Msg = Increment | Decrement | Reset
+
+type Msg
+    = Increment
+    | Decrement
+    | Reset
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment -> model + 1
-    Decrement -> model - 1
-    Reset -> 0
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+        Reset ->
+            0
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , button [ onClick Increment ] [ text "+" ]
-    , button [onClick Reset] [text "Reset"]
-    , div [] [ text (toString model) ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "Reset" ]
+        , div [] [ text (toString model) ]
+        ]
