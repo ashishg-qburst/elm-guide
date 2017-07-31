534b2864a41bf6d73d4a26e9e1bce85d8fb1c2d0
ash <ashishg@qburst.com>
2017-07-31 11:21:04 +0530

    Add 'Reset' button for counter

--- button.elm
+++ button.elm
@@ -8,18 +8,20 @@ type alias Model = Int
 model : Model
 model = 0
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment -> model + 1
     Decrement -> model - 1
+    Reset -> 0
 
 view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [onClick Reset] [text "Reset"]
+    , div [] [ text (toString model) ]
     ]
